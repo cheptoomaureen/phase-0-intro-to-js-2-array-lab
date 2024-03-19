be77fd5bc86cc3244e2ad09f2875cbe78afd652a
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-19 04:48:52 +0300

    assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,56 +1,4 @@
 require("./helpers.js");
-const cats = ["Milo", "Otis", "Garfield"];
-
-function destructivelyAppendCat(name) {
-  cats.push(name);
-}
-
-function destructivelyPrependCat(name) {
-  cats.unshift(name);
-}
-
-function destructivelyRemoveLastCat() {
-  cats.pop();
-}
-
-function destructivelyRemoveFirstCat() {
-  cats.shift();
-}
-
-function appendCat(name) {
-  return [...cats, name];
-}
-function destructivelyAppendCat(name) {
-  cats.push(name);
-}
-
-function destructivelyPrependCat(name) {
-  cats.unshift(name);
-}
-
-function destructivelyRemoveLastCat() {
-  cats.pop();
-}
-
-function destructivelyRemoveFirstCat() {
-  cats.shift();
-}
-
-function appendCat(name) {
-  return [...cats, name];
-}
-
-function prependCat(name) {
-  return [name, ...cats];
-}
-
-function removeLastCat() {
-  return cats.slice(0, -1);
-}
-
-function removeFirstCat() {
-  return cats.slice(1);
-}
 
 describe("index.js", function () {
   describe("cats", function () {
@@ -58,6 +6,7 @@ describe("index.js", function () {
       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
     });
   });
+
 
   describe("Array functions", function () {
     beforeEach(function () {
